b9da0ebc50c3f6ba91f1e3bd03efa44c6d4eeab7
genivalvb <@OphiuchusGI007>
2022-11-06 17:58:52 -0300

    Database added

---
+++ src/main/java/pi32022_rota_intestimentos/RotaInvestimentosApplication.java
@@ -0,0 +1,14 @@
+package pi32022_rota_intestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaInvestimentosApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RotaInvestimentosApplication.class, args);
+
+    }
+}
